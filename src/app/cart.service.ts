624b430f7fbd2eb6fc781a6182b6d0e6214e30c4
shahreyarbutt <115699632+shahreyarbutt@users.noreply.github.com>
2023-03-22 14:57:40 +0500

    4 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -10,7 +10,7 @@ export class CartService {
     this.items.push(product);
   }
 
-  getCartItems(product: Product) {
+  getCartItems() {
     this.items;
   }
 
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  items = this.cartService.getCartItems();
 
+  constructor(private cartService: CartService) {}
 }
